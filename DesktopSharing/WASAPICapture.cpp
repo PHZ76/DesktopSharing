550763c5bd350aaf04da1c166760c979fe56d745
PHZ76 <2235710879@qq.com>
2019-08-12 23:40:21 -0700

    add core audio capture

---
+++ DesktopSharing/WASAPICapture.cpp
@@ -0,0 +1,206 @@
+#include "WASAPICapture.h"
+
+WASAPICapture::WASAPICapture()
+	: m_initialized(false)
+	, m_isEnabeld(false)
+	, m_mixFormat(NULL)
+{
+	
+}
+
+WASAPICapture::~WASAPICapture()
+{
+	if (m_initialized)
+	{
+		CoUninitialize();
+	}
+}
+
+int WASAPICapture::init()
+{
+	std::lock_guard<std::mutex> locker(m_mutex);
+
+	if (m_initialized)
+	{
+		return 0;
+	}
+
+	CoInitialize(NULL);
+
+	HRESULT hr = S_OK;
+	hr = CoCreateInstance(CLSID_MMDeviceEnumerator, NULL, CLSCTX_ALL, IID_IMMDeviceEnumerator, (void**)m_enumerator.GetAddressOf());
+	if (FAILED(hr)) 
+	{
+		printf("[WASAPICapture] Failed to create instance.\n");
+		return -1;
+	}
+
+	hr = m_enumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, m_device.GetAddressOf());
+	if (FAILED(hr)) 
+	{
+		printf("[WASAPICapture] Failed to create device.\n");
+		return -1;
+	}
+
+	hr = m_device->Activate(IID_IAudioClient, CLSCTX_ALL, NULL, (void**)m_audioClient.GetAddressOf());
+	if (FAILED(hr)) 
+	{
+		printf("[WASAPICapture] Failed to activate device.\n");
+		return -1;
+	}
+
+	hr = m_audioClient->GetMixFormat(&m_mixFormat);
+	if (FAILED(hr)) 
+	{
+		printf("[WASAPICapture] Failed to get mix format.\n");
+		return -1;
+	}
+
+	m_hnsActualDuration = REFTIMES_PER_SEC;
+	hr = m_audioClient->Initialize(AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_LOOPBACK, m_hnsActualDuration, 0, m_mixFormat, NULL);
+	if (FAILED(hr)) 
+	{
+		printf("[WASAPICapture] Failed to initialize audio client.\n");
+		return -1;
+	}
+
+	hr = m_audioClient->GetBufferSize(&m_bufferFrameCount);
+	if (FAILED(hr)) 
+	{
+		printf("[WASAPICapture] Failed to get buffer size.\n");
+		return -1;
+	}
+
+	hr = m_audioClient->GetService(IID_IAudioCaptureClient, (void**)m_audioCaptureClient.GetAddressOf());
+	if (FAILED(hr)) 
+	{
+		printf("[WASAPICapture] Failed to get service.\n");
+		return -1;
+	}
+
+	// Calculate the actual duration of the allocated buffer.
+	m_hnsActualDuration = REFERENCE_TIME(REFTIMES_PER_SEC * m_bufferFrameCount / m_mixFormat->nSamplesPerSec);
+
+	m_initialized = true;
+	return 0;
+}
+
+int WASAPICapture::exit()
+{
+	return 0;
+}
+
+int WASAPICapture::start()
+{
+	std::lock_guard<std::mutex> locker(m_mutex);
+
+	if (!m_initialized)
+	{
+		return -1;
+	}
+
+	if (m_isEnabeld)
+	{
+		return 0;
+	}
+
+	HRESULT hr = m_audioClient->Start();
+	if (FAILED(hr))
+	{
+		printf("[WASAPICapture] Failed to start audio client.\n");
+		return -1;
+	}
+	
+	m_isEnabeld = true;
+	m_threadPtr.reset(new std::thread([this] {
+
+
+		while (this->m_isEnabeld)
+		{			
+
+			if (this->capture() < 0)
+			{
+				break;
+			}
+		}
+	}));
+
+	return 0;
+}
+
+int WASAPICapture::stop()
+{
+	std::lock_guard<std::mutex> locker(m_mutex);
+	if (m_isEnabeld)
+	{
+		m_isEnabeld = false;
+		m_threadPtr->join();
+
+		HRESULT hr = m_audioClient->Stop();
+		if (FAILED(hr))
+		{
+			printf("[WASAPICapture] Failed to stop audio client.\n");
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+void WASAPICapture::setCallback(PacketCallback callback)
+{
+	std::lock_guard<std::mutex> locker(m_mutex2);
+	m_callback = callback;
+}
+
+int WASAPICapture::capture()
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+	HRESULT hr = S_OK;
+	uint32_t packetLength = 0;
+	uint32_t numFramesAvailable = 0;
+	BYTE *pData;
+	DWORD flags;
+
+	hr = m_audioCaptureClient->GetNextPacketSize(&packetLength);
+	if (FAILED(hr)) 
+	{
+		printf("[WASAPICapture] Faild to get next data packet size.\n");
+		return -1;
+	}
+
+	while (packetLength != 0) 
+	{
+		hr = m_audioCaptureClient->GetBuffer(&pData, &numFramesAvailable, &flags, NULL, NULL);
+		if (FAILED(hr)) 
+		{
+			printf("[WASAPICapture] Faild to get buffer.\n");
+			return -1;
+		}
+
+		{
+			std::lock_guard<std::mutex> locker(m_mutex2);
+			if (m_callback)
+			{
+				m_callback(m_mixFormat, pData, numFramesAvailable);
+			}
+		}
+
+		hr = m_audioCaptureClient->ReleaseBuffer(numFramesAvailable);
+		if (FAILED(hr)) 
+		{
+			printf("[WASAPICapture] Faild to release buffer.\n");
+			return -1;
+		}
+
+		hr = m_audioCaptureClient->GetNextPacketSize(&packetLength);
+		if (FAILED(hr)) 
+		{
+			printf("[WASAPICapture] Faild to get next data packet size.\n");
+			return -1;
+		}
+	}
+
+	return 0;
+}
---
+++ DesktopSharing/WASAPICapture.h
@@ -0,0 +1,54 @@
+// https://docs.microsoft.com/en-us/previous-versions//ms678709(v=vs.85)
+
+#ifndef WASAPI_CAPTURE_H
+#define WASAPI_CAPTURE_H
+
+#include <Audioclient.h>
+#include <mmdeviceapi.h>
+#include <wrl.h>
+#include <cstdio>
+#include <cstdint>
+#include <mutex>
+#include <thread>
+
+class WASAPICapture
+{
+public:
+	typedef std::function<void(WAVEFORMATEX *m_mixFormat, BYTE *data, uint32_t samples)> PacketCallback;
+
+	WASAPICapture();
+	~WASAPICapture();
+	WASAPICapture &operator=(const WASAPICapture &) = delete;
+	WASAPICapture(const WASAPICapture &) = delete;
+
+	int init();
+	int exit();
+	int start();
+	int stop();
+	void setCallback(PacketCallback callback);
+
+private:
+	int capture();
+
+	const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+	const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+	const IID IID_IAudioClient = __uuidof(IAudioClient);
+	const IID IID_IAudioCaptureClient = __uuidof(IAudioCaptureClient);
+	const int REFTIMES_PER_SEC = 10000000;
+	const int REFTIMES_PER_MILLISEC = 10000;
+
+	bool m_initialized;
+	bool m_isEnabeld;
+	std::mutex m_mutex, m_mutex2;
+	std::shared_ptr<std::thread> m_threadPtr;
+	WAVEFORMATEX *m_mixFormat;
+	REFERENCE_TIME m_hnsActualDuration;
+	uint32_t m_bufferFrameCount;
+	PacketCallback m_callback;
+	Microsoft::WRL::ComPtr<IMMDeviceEnumerator> m_enumerator;
+	Microsoft::WRL::ComPtr<IMMDevice> m_device;
+	Microsoft::WRL::ComPtr<IAudioClient> m_audioClient;
+	Microsoft::WRL::ComPtr<IAudioCaptureClient> m_audioCaptureClient;
+};
+
+#endif
