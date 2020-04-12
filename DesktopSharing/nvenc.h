f24353e07e72c80f98d4f434f0bfe9204b4b5fea
“penghaoze <penghaoze@kingsoft.com>
2020-04-12 20:01:03 +0800

    update

--- DesktopSharing/nvenc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _NVIDIA_ENCODER_H
-#define _NVIDIA_ENCODER_H
-
-#include "encoder_info.h"
-#include "NvEncoderD3D11.h" // Video-Codec-SDK Version: 8.2
-
-extern struct encoder_info nvenc_info;
-
-#endif
